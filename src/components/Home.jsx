cea7848d5c69382ef2a29909e0a29167d9333483
hreinn <hreinngy@gmail.com>
2023-02-26 19:49:20 +0100

    Adds React router with routes. Adds Navbar and all components needed

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1>Welcome to SocialMedia</h1>
+      <h2>The best Social media</h2>
+    </>
+  );
+}
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home.jsx';
+import SignIn from './components/SignIn.jsx';
+import SignUp from './components/SignUp.jsx';
+import Posts from './components/Posts.jsx';
+import CreatePost from './components/CreatePost.jsx';
+import Profile from './components/Profile.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/signin',
+        element: <SignIn />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/posts',
+        element: <Posts />,
+      },
+      {
+        path: '/createpost',
+        element: <CreatePost />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
