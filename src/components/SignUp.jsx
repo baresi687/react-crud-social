eadb5976fb868c2d3c10a7a843ab13b2f56c19e3
hreinn <hreinngy@gmail.com>
2023-02-26 20:51:11 +0100

    Adds html skeleton and styling for forms

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -2,6 +2,27 @@ function SignUp() {
   return (
     <>
       <h1>Sign Up</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" placeholder="Username" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" placeholder="Email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" placeholder="Password" />
+        </div>
+        <div>
+          <label htmlFor="confirm-password">Confirm password</label>
+          <input id="confirm-password" type="password" placeholder="Confirm password" />
+        </div>
+        <div>
+          <input id="confirm-password" type="submit" placeholder="Submit" value="Submit" />
+        </div>
+      </form>
     </>
   );
 }
