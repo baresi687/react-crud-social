6315d43fe1b12b2d653e38eb7d33aa89fc008951
hreinn <hreinngy@gmail.com>
2023-02-27 10:11:23 +0100

    Adds Controlled components on Form inputs

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,25 +1,62 @@
 import Button from './Button.jsx';
+import { useState } from 'react';
 
 function SignUp() {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  function onTextInputChange(value, setTarget) {
+    setTarget(value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" placeholder="Username" />
+          <input
+            name="username"
+            value={userName}
+            placeholder="Username"
+            onChange={(e) => onTextInputChange(e.target.value, setUserName)}
+          />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" placeholder="Email" />
+          <input
+            name="email"
+            value={email}
+            type="email"
+            placeholder="Email"
+            onChange={(e) => onTextInputChange(e.target.value, setEmail)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" placeholder="Password" />
+          <input
+            name="password"
+            value={password}
+            type="password"
+            placeholder="Password"
+            onChange={(e) => onTextInputChange(e.target.value, setPassword)}
+          />
         </div>
         <div>
           <label htmlFor="confirm-password">Confirm password</label>
-          <input id="confirm-password" type="password" placeholder="Confirm password" />
+          <input
+            name="confirm-password"
+            value={confirmPassword}
+            type="password"
+            placeholder="Confirm password"
+            onChange={(e) => onTextInputChange(e.target.value, setConfirmPassword)}
+          />
         </div>
         <div>
           <Button type="submit" color="#3f51b5bf">
