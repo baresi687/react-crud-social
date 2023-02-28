546d82d4430cef33b842c6b94cbe7350aae5e1aa
hreinn <hreinngy@gmail.com>
2023-02-28 15:43:37 +0100

    Moves global scss files to sass folder with partials

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import SignIn from './components/SignIn.jsx';
